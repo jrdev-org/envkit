b07db58834a67e60be2ec021b9222255ce478c6a
Elly B <baxllycc@gmail.com>
2025-09-16 09:45:14 +0300

    feat(web) : SIGNIN component added

---
+++ apps/web/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from "@clerk/nextjs";
+import { env } from "~/env.js";
+
+export default function SignInPage() {
+  return (
+    <SignIn
+      forceRedirectUrl={env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL}
+    />
+  );
+}
