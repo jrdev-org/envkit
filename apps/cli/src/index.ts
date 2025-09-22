dbe3a0f1ba1122bb3e1edcc92d21e1e7309fb4b4
Elly B <baxllycc@gmail.com>
2025-09-22 10:02:53 +0300

    feat(cli): added gitignore and db test

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import { dbApi } from "@envkit/db";
+
+try {
+  const me = await dbApi.users.get(process.env.TEST_USER_ID!);
+  console.table(me);
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(
+      error.message.includes("fetch failed") ? "Network error" : error.message
+    );
+  } else {
+    console.log(error);
+  }
+}
